1778315a21c5fd5c006941f42e179936f6c45afa
Charlie <wl00606352@gmail.com>
2021-07-13 01:48:04 +0800

    project init

---
+++ config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/pet-cord', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true
+})
+
+const db = mongoose.connection
+
+db.on('error', console.log('Ooooops, something wrong with mongodb...'))
+db.once('open', console.log('mongodb connected!'))
+
+module.exports = db
