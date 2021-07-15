348b048a5b5366b15d30f34849813dee5a5a335a
Charlie <wl00606352@gmail.com>
2021-07-16 03:00:57 +0800

    feat: add upload avatar function

--- routes/modules/pets.js
+++ routes/modules/pets.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
+const multer = require('multer')
+const upload = multer({ dest: 'temp/' })
+
 const petsController = require('../../controller/petsController')
 
 router.get('/', petsController.getPets)
-router.post('/', petsController.addPet)
-router.put('/:petId', petsController.updatePet)
+router.post('/', upload.single('avatar'), petsController.addPet)
+router.put('/:petId', upload.single('avatar'), petsController.updatePet)
 router.delete('/:petId', petsController.deletePet)
 
 module.exports = router
