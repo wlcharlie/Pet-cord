1ca9c442464b1bcc9b02d6b40393d50f16b68637
Charlie <wl00606352@gmail.com>
2021-08-20 02:46:48 +0800

    feat: cors setting

---
+++ config/cors.js
@@ -0,0 +1,12 @@
+const cors = require("cors")
+
+module.exports = () => {
+  const whiteList = process.env.CORS_WHITE_LIST.split(",")
+  const corsOptionsDelegate = (req, callback) => {
+    if (whiteList.indexOf(req.header("Origin")) !== -1) {
+      return callback(null, { origin: true })
+    }
+    return callback(null, { origin: false })
+  }
+  return cors(corsOptionsDelegate)
+}
