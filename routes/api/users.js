d060ba0a1626fca5a065da20913db8a0525d85c7
Charlie <wl00606352@gmail.com>
2021-08-22 17:09:09 +0800

    minor fixing

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,6 @@ const upload = multer({ dest: "temp/" })
 const usersController = require("../../controller/api/usersController")
 
 router.get("/:UID", usersController.getUser)
-router.post("/", upload.single("avatar"), usersController.addUser)
+router.post("/", usersController.addUser)
 
 module.exports = router
