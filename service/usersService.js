28c5e1fdbc6b2795cbd262376f1e60399e08818b
Charlie <wl00606352@gmail.com>
2021-08-22 19:09:53 +0800

    return the new create user info

--- service/usersService.js
+++ service/usersService.js
@@ -6,8 +6,8 @@ const usersService = {
   },
 
   addUser: async data => {
-    await User.create(data)
-    return { status: "success", message: "a new user data has created" }
+    return await User.create(data).lean()
+    // return { status: "success", message: "a new user data has created" }
   },
 
   updateUser: async () => {},
