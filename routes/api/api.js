741279f52f8317e91456105ef1803813d8c7c242
Charlie <wl00606352@gmail.com>
2021-08-25 21:05:09 +0800

    type

--- routes/api/api.js
+++ routes/api/api.js
@@ -6,6 +6,6 @@ const users = require("./users")
 const healths = require("./healths")
 router.use("/pets", pets)
 router.use("/users", users)
-router.use("/health", healths)
+router.use("/healths", healths)
 
 module.exports = router
